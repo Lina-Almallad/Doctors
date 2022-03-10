0bf35fa142fd1e0bf215f05ee032740800edcf82
Lina <linaalmallad>
2022-03-10 14:47:34 +0300

    Create system for doctors
    Create a system for doctors to print their roles

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,5 @@
 package com.company;
 
-interface Doctor{
-    public void viewProfile();
-}
-class Dentist implements Doctor{
-    public void viewProfile(){
-        System.out.println("Dentist");
-    }
-}
-class Psychologist implements Doctor{
-    public void viewProfile(){
-        System.out.println("Psychologist");
-    }
-}
-class Pediatrician implements Doctor{
-    public void viewProfile(){
-        System.out.println("Pediatrician");
-    }
-}
 public class Main {
 
     public static void main(String[] args) {
